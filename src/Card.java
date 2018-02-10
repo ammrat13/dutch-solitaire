1c131927290f390362d50bd5f5245be6abbaec2b
ammrat13 <ammrat13@gmail.com>
2018-02-09 18:34:38 -0600

    Added cards

---
+++ src/Card.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+/**
+This file represents an individual card. It has utilities for processing card 
+numbers and suits and also stores the image of the card. Note: {@code 0} is 
+Ace, {@code 1} is 2 ..., {@code 9} is 10, {@code 10} is Jack, {@code 11} is 
+Queen, and {@code 12} is King.
+*/
+public class Card {
+
+	/** Stores the root directorry of the images */
+	private final String IMGROOT = "../img/";
+
+	/** Static variable for the clubs suit. */
+	public final static int CLUBS = 0;
+	/** Static variable for the diamonds suit. */
+	public final static int DIAMONDS = 1;
+	/** Static variable for the hearts suit. */
+	public final static int HEARTS = 2;
+	/** Static variable for the spades suit. */
+	public final static int SPADES = 3;
+
+	/** Instance variable storing the number of the card. */
+	public final int num;
+	/** Instance variable storing the suit of the card. */
+	public final int suit;
+	/** Instance variable storing the image of the card. */
+	public final BufferedImage img;
+
+	/**
+	Constructs the card itself
+	@param n The number of the card
+	@param s The suit of the card
+	*/
+	public Card(int n, int s) throws IllegalArgumentException {
+		// Error checking: n is on [0,12] and s is on [0,3]
+		if(n < 0 || n > 12 || s < 0 || s > 3)
+			throw new IllegalArgumentException("Invalid card.");
+
+		num = n;
+		suit = s;
+
+		BufferedImage temp = null;
+		try {
+			temp = ImageIO.read(new File(getImgPath(n, s)));
+		} catch (IOException e){
+			temp = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+			e.printStackTrace();
+		}
+		img = temp;
+	}
+
+	/**
+	Gets the path for the image of the specified number and suit.
+	@param n The number of the card
+	@param s The suit of the card
+	@return The path to the image of that card
+	*/
+	private String getImgPath(int n, int s){
+		String ret = IMGROOT;
+
+		n++;
+		switch(n){
+			case 1:
+				ret += "A"; break;
+			case 11:
+				ret += "J"; break;
+			case 12:
+				ret += "Q"; break;
+			case 13:
+				ret += "K"; break;
+			default:
+				ret += n;
+		}
+
+		switch(s){
+			case CLUBS:
+				ret += "C"; break;
+			case DIAMONDS:
+				ret += "D"; break;
+			case HEARTS:
+				ret += "H"; break;
+			case SPADES:
+				ret += "S"; break;
+		}
+		ret += ".png";
+		
+		return ret;
+	}
+
+}
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.io.*;
+import java.util.*;
 import javax.imageio.*;
 import javax.swing.*;
 
@@ -14,6 +15,16 @@ public class GamePanel extends JPanel {
 	/** The height of the panel. */
 	private final int HEIGHT;
 
+	/** The margin on each side in pixels. */
+	private final int MAR = 50;
+	/** The ratio of the cards' length to their width. */
+	private final double AR = 1056.0/691.0;
+	/** The margin around each card in pixels. */
+	private final int CMAR = 5;
+
+	/** Stores the cards we have on the table. */
+	Card[][] cs = new Card[4][14];
+
 	/**
 	Constructs the panel itself.
 	@param w The width of the panel
@@ -22,6 +33,39 @@ public class GamePanel extends JPanel {
 	public GamePanel(int w, int h){
 		WIDTH = w;
 		HEIGHT = h;
+		reset();
+	}
+
+	/**
+	Resets the game. Resuffles the deck and redeals the cards.
+	*/
+	private void reset(){
+		// Empty the current array. We only empty the last column as the rest 
+		//	will be overwritten
+		for(int r=0; r<4; r++)
+			cs[r][13] = null;
+
+
+		// Fill the array randomly
+		ArrayList<Card> ctemp = new ArrayList<Card>();
+		for(int n=0; n<13; n++)
+			for(int s=0; s<4; s++)
+				ctemp.add(new Card(n, s));
+		Collections.shuffle(ctemp);
+		for(int r=0; r<4; r++)
+			for(int c=0; c<13; c++)
+				cs[r][c] = ctemp.get(r*13+c);
+	}
+
+	/**
+	Moves the card at the specified index to the specified empty space.
+	@param ri The row of the card to move
+	@param ci The column of the card to move
+	@param rf The row to the empty space
+	@param cf The column of the empty space
+	*/
+	private void swap(int ri, int ci, int rf, int cf){
+		// TODO: implement swap code
 	}
 
 	/** {@inheritdoc} */
@@ -33,11 +77,16 @@ public class GamePanel extends JPanel {
 		g2d.setColor(Color.WHITE);
 		g2d.drawRect(0, 0, WIDTH, HEIGHT);
 
-		try {
-			// Draw one card
-			g2d.drawImage(ImageIO.read(new File("../img/2C.png")), 10, 10, 100, 153, null);
-		} catch(IOException e){
-			e.printStackTrace();
+		// Draw the grid and the card
+		g2d.setColor(Color.BLACK);
+		int gW = (WIDTH - 2*MAR)/14;
+		int gH = (int) ((gW - 2*CMAR)*AR + 2*CMAR);
+		for(int r=0; r<4; r++){
+			for(int c=0; c<14; c++){
+				g2d.drawRect(c*gW + MAR, r*gH + MAR, gW, gH);
+				if(cs[r][c] != null)
+					g2d.drawImage(cs[r][c].img, c*gW + MAR + CMAR, r*gH + MAR + CMAR, gW - 2*CMAR, gH - 2*CMAR, null, null);
+			}
 		}
 	}
 
