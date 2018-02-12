7ba10b2f54a28f7365fce0ed9b7266ad5fa130ca
ammrat13 <ammrat13@gmail.com>
2018-02-12 12:54:45 -0600

    Made everything except the win condition

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -31,6 +31,11 @@ public class GamePanel extends JPanel implements KeyListener {
 	/** Column of the active card. */
 	int cAct = 0;
 
+	/** Row of the selected card. Negative one if no card is selected. */
+	int rSel = -1;
+	/** Column of the selected card. Negative one if no card is selected. */
+	int cSel = -1;
+
 	/** Stores the keys that are currently down so we don't count twice. */
 	Set<Integer> kDs = new HashSet<>();
 
@@ -81,12 +86,17 @@ public class GamePanel extends JPanel implements KeyListener {
 	@param cf The column of the empty space
 	*/
 	private void swap(int ri, int ci, int rf, int cf){
+		try{
+			System.out.println(cs[ri][ci]);
+			System.out.println(cs[rf][cf+1]);
+			System.out.println(cs[rf][cf-1]);
+		} catch(Exception e){}
 		try {
 			if(
 				((cs[ri][ci].num == 12 && cf == 13) // Aces can be swapped to the last row
 					|| (cs[ri][ci].num == 0 && cf == 0) // Twos can be swapped to the first row
-					|| (cs[ri][ci].num == cs[ri][ci+1].num - 1 && cs[ri][ci].suit == cs[ri+1][ci].suit) // Right order swap
-					|| (cs[ri][ci].num == cs[ri][ci-1].num + 1 && cs[ri][ci].suit == cs[ri-1][ci].suit)) // Left order swap
+					|| (cs[ri][ci].num == cs[rf][cf+1].num - 1 && cs[ri][ci].suit == cs[rf][cf+1].suit) // Right order swap
+					|| (cs[ri][ci].num == cs[rf][cf-1].num + 1 && cs[ri][ci].suit == cs[rf][cf-1].suit)) // Left order swap
 				&& (cs[rf][cf] == null && cs[ri][ci] != null)	// End position is null, but start is not
 			){
 				cs[rf][cf] = cs[ri][ci];
@@ -114,6 +124,10 @@ public class GamePanel extends JPanel implements KeyListener {
 				// Draw the border
 				g2d.setColor(Color.BLACK);
 				g2d.drawRect(c*gW + MAR, r*gH + MAR, gW, gH);
+				// Draw a highlight if it is selected
+				g2d.setColor(Color.RED);
+				if(r == rSel && c == cSel)
+					g2d.fillRect(c*gW + MAR + 1, r*gH + MAR + 1, gW - 1, gH - 1);
 				// Draw a highlight if it is active
 				g2d.setColor(Color.BLUE);
 				if(r == rAct && c == cAct)
@@ -128,10 +142,34 @@ public class GamePanel extends JPanel implements KeyListener {
 	/** {@inheritdoc} */
 	@Override
 	public void keyPressed(KeyEvent e){
-		if(e.getKeyCode() == KeyEvent.VK_ESCAPE && !kDs.contains(e.getKeyCode())){
-			kDs.add(e.getKeyCode());
+		if(isKeyPressed(e, KeyEvent.VK_ESCAPE)){
 			reset();
 		}
+
+		if(isKeyPressed(e, KeyEvent.VK_UP)){
+			rAct = Math.max(rAct-1, 0);
+		}
+		if(isKeyPressed(e, KeyEvent.VK_DOWN)){
+			rAct = Math.min(rAct+1, 3);
+		}
+		if(isKeyPressed(e, KeyEvent.VK_LEFT)){
+			cAct = Math.max(cAct-1, 0);
+		}
+		if(isKeyPressed(e, KeyEvent.VK_RIGHT)){
+			cAct = Math.min(cAct+1, 13);
+		}
+
+		if(isKeyPressed(e, KeyEvent.VK_ENTER)){
+			if(rSel < 0 || cSel < 0){
+				rSel = rAct;
+				cSel = cAct;
+			} else {
+				swap(rSel, cSel, rAct, cAct);
+				rSel = -1;
+				cSel = -1;
+			}
+		}
+
 		repaint();
 	}
 
@@ -145,6 +183,21 @@ public class GamePanel extends JPanel implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e){
 
+	}
+
+	/**
+	Handles the logic of checking if a key is pressed. Also adds the key to the 
+	set of pressed keys if it is pressed.
+	@param e The key event
+	@param k The key to check
+	@return If the key is pressed and hasn't triggered yet
+	*/
+	private boolean isKeyPressed(KeyEvent e, int k){
+		if(e.getKeyCode() == k && !kDs.contains(e.getKeyCode())){
+			kDs.add(e.getKeyCode());
+			return true;
+		}
+		return false;
 	}
 
 }
